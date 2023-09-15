18609eee25793666d44262772e338f723e37548e
gdanichev <GDanichev@artenecy.ru>
2023-09-15 18:13:15 +0300

    filestorage

--- cmd/storage/main.go
+++ cmd/storage/main.go
@@ -20,7 +20,16 @@ func main() {
 	server := http.Server{Addr: ":8000", ReadTimeout: time.Second * 3}
 
 	fileStorage := memoryfilestorage.NewStorage()
-	http.HandleFunc("/file", httplog.Log(api.SaveFile(fileStorage)))
+	http.HandleFunc("/file", httplog.Log(func(writer http.ResponseWriter, request *http.Request) {
+		switch request.Method {
+		case "GET":
+			api.GetFile(fileStorage)(writer, request)
+		case "POST":
+			api.SaveFile(fileStorage)(writer, request)
+		default:
+			http.Error(writer, fmt.Sprintf("method %s is not supported", request.Method), http.StatusBadRequest)
+		}
+	}))
 
 	go func() {
 		err := server.ListenAndServe()
--- internal/filestorage/api/api.go
+++ internal/filestorage/api/api.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/yonesko/s3-test-task/internal/filestorage/service"
 	"github.com/yonesko/s3-test-task/internal/model"
+	"io"
+	"log"
 	"net/http"
 )
 
@@ -27,6 +29,31 @@ func SaveFile(fileStorageService service.FileStorage) http.HandlerFunc {
 			http.Error(writer, err.Error(), http.StatusInternalServerError)
 			return
 		}
+		writer.WriteHeader(http.StatusOK)
+	}
+}
+
+func GetFile(fileStorageService service.FileStorage) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		if request.Method != "GET" {
+			http.Error(writer, fmt.Sprintf("method %s is not supported", request.Method), http.StatusBadRequest)
+			return
+		}
+
+		file, err := fileStorageService.GetFile(request.Context(), request.URL.Query().Get("name"))
+		//TODO file closer
+		if err != nil {
+			log.Default().Printf("GetFile err: %s", err)
+			writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		_, err = io.Copy(writer, file)
+		if err != nil {
+			log.Default().Printf("Copy err: %s", err)
+			writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
 		writer.WriteHeader(http.StatusOK)
 	}
 }
