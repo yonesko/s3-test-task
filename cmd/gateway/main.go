1c5eded2e25137e82ac6d267d9c5c958a6ef9354
gdanichev <GDanichev@artenecy.ru>
2023-09-15 16:38:13 +0300

    init

---
+++ cmd/gateway/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	mainCtx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGINT)
+	defer stop()
+
+	server := http.Server{Addr: ":8000", ReadTimeout: time.Second * 3}
+
+	//var fg service.FileGateway
+
+	http.HandleFunc("/file", func(writer http.ResponseWriter, request *http.Request) {
+		if request.Method == "GET" {
+
+		}
+	})
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			fmt.Println("ListenAndServe err:", err)
+		}
+	}()
+
+	select {
+	case <-mainCtx.Done():
+		err := server.Shutdown(context.Background())
+		if err != nil {
+			fmt.Println("Shutdown err:", err)
+		}
+		return
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yonesko/s3-test-task
+
+go 1.21.1
